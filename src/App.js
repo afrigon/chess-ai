bff58bb4ff620cf42927442d5f3fb73607a979b1
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2019-11-19 22:38:18 -0500

    does best move for the round

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Chess from "chess.js";
 import Chessboard from "chessboardjsx";
+import ChessWeight from "./ChessWeight";
 
 export default function App() {
     const [game, setGame] = useState(new Chess());
@@ -10,15 +11,58 @@ export default function App() {
 
     const randomMove = () => {
         const moves = game.moves();
-
         if (game.game_over() || game.in_draw() || moves.length === 0) return;
+
         const i = Math.floor(Math.random() * moves.length);
         game.move(moves[i]);
         setFen(game.fen());
         setGame(game);
     }
 
-    const makeMove = randomMove
+    const greedyMove = () => {
+        const moves = game.moves();
+        if (game.game_over() || game.in_draw() || moves.length === 0) return;
+
+        let best = 9999;
+        let bestMove;
+        for (let i = 0; i < moves.length; ++i) {
+            game.move(moves[i]);
+            const value = evalBoard(game);
+            
+            if (value < best) {
+                best = value;
+                bestMove = moves[i];
+            }
+
+            game.undo();
+        }
+
+        game.move(bestMove);
+
+        setFen(game.fen());
+        setGame(game);
+    }
+
+    const makeMove = greedyMove
+
+    const evalBoard = g => {
+        const columns = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+        let score = 0;
+        for (var i = 0; i < 8; ++i) {
+            for (var j = 0; j < 8; ++j) {
+                score += evalPiece(g.get(columns[i] + (8-j)), i ,j);
+            }
+        }
+        return score;
+    }
+
+    const evalPiece = (p, x, y) => {
+        if (!p) return 0;
+
+        const value = ChessWeight[p.type][p.color][y][x] + ChessWeight[p.type].modifier;
+        return p.color === "w" ? value : -value;
+    }
 
     const onDrop = ({ sourceSquare, targetSquare }) => {
         const move = game.move({
@@ -26,6 +70,7 @@ export default function App() {
             to: targetSquare,
             promotion: "q"
         });
+
         if (!move) return;
 
         return new Promise(resolve => {
