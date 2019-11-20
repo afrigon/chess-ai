fdc7929bc38fb15568c663aa864444540dd56a28
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2019-11-19 20:09:27 -0500

    random move working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hello world</h1>
-      </header>
-    </div>
-  );
+import Chess from "chess.js";
+import Chessboard from "chessboardjsx";
+
+export default function App() {
+    const [game, setGame] = useState(new Chess());
+    const [fen, setFen] = useState("start");
+
+    const randomMove = () => {
+        const moves = game.moves();
+
+        if (game.game_over() || game.in_draw() || moves.length === 0) return;
+        const i = Math.floor(Math.random() * moves.length);
+        game.move(moves[i]);
+        setFen(game.fen());
+        setGame(game);
+    }
+
+    const makeMove = randomMove
+
+    const onDrop = ({ sourceSquare, targetSquare }) => {
+        const move = game.move({
+            from: sourceSquare,
+            to: targetSquare,
+            promotion: "q"
+        });
+        if (!move) return;
+
+        return new Promise(resolve => {
+            setFen(game.fen());
+            setGame(game);
+            resolve();
+        }).then(() => setTimeout(makeMove, 300));
+    }
+
+    return (
+        <div className="App">
+            <Chessboard
+                width={540}
+                position={fen}
+                onDrop={onDrop}
+                transitionDuration={300}
+                boardStyle={{ borderRadius: "5px" }}
+            />
+        </div>
+    );
 }
 
-export default App;
