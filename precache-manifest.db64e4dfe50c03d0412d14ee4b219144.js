cebb33952be7320743eb1df786918fe269b4077e
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2019-11-21 15:46:51 -0500

    Updates

---
+++ precache-manifest.db64e4dfe50c03d0412d14ee4b219144.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5b1b9a070efe14a959c11c4ec8da71",
+    "url": "/chess-ai/index.html"
+  },
+  {
+    "revision": "9b1c0cfd90c7bc19eb94",
+    "url": "/chess-ai/static/css/main.e0279cf8.chunk.css"
+  },
+  {
+    "revision": "77ed5240c663b1474d5f",
+    "url": "/chess-ai/static/js/2.8a66f553.chunk.js"
+  },
+  {
+    "revision": "9b1c0cfd90c7bc19eb94",
+    "url": "/chess-ai/static/js/main.cb65d897.chunk.js"
+  },
+  {
+    "revision": "0fe1c199a7dbeb6d2bbf",
+    "url": "/chess-ai/static/js/runtime-main.293ff757.js"
+  }
+]);
